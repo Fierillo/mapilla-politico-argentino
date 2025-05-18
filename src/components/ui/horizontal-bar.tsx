284c9181fe7bd0eaf94c3fde7ab8692a83152bf0
fierillo <fierillo@protonmail.com>
2025-05-18 14:49:14 -0300

    style: enhance HorizontalBar component with interactive party details display

--- src/components/ui/horizontal-bar.tsx
+++ src/components/ui/horizontal-bar.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/src/components/ui/tooltip";
 import { provinces } from "@/src/data/provinces";
+import { useState } from "react";
 
 type Party = "peronismo" | "ucr" | "pro" | "liberales";
 type BancasType = 'bancas' | 'loseBancas' | 'renewBancas'
+
 const parties: Party[] = ["peronismo", "liberales", "ucr", "pro"];
 const colorMap: Record<Party, string> = {
   peronismo: "sky",
@@ -19,6 +15,7 @@ const colorMap: Record<Party, string> = {
 };
 
 export default function HorizontalBar() {
+  const [selectedParty, setSelectedParty] = useState<Party | null>(null);
   
   function sumBancas (party: Party, bancasType: BancasType) {
     return provinces.reduce(
@@ -29,29 +26,28 @@ export default function HorizontalBar() {
       },
       0
     );
-  }
-
+  }  
+  
   const totalLoseBancas = (
     sumBancas('liberales','loseBancas')
     + sumBancas('peronismo','loseBancas')
     + sumBancas('pro','loseBancas')
     + sumBancas('ucr','loseBancas')
-  )
-
+  )  
+  
   const totalBancas = (
     sumBancas('liberales','bancas')
     + sumBancas('peronismo','bancas')
     + sumBancas('pro','bancas')
     + sumBancas('ucr','bancas')
-  )
-
+  )  
+  
   return (
     <div className="w-full max-w-3xl mb-4">
-      <h2 className="md:text-xl text-md mb-2 text-center">
-        Bancas totales: {totalBancas} | En juego: {totalLoseBancas}
+      <h2 className="text-lg font-semibold mb-2 text-center">
+        Bancas Totales en Juego (2025): {totalLoseBancas} / {totalBancas}
       </h2>
-      <TooltipProvider>
-      <div className="flex h-6 rounded-lg overflow-hidden border border-gray-300">
+      <div className="flex h-6 border border-gray-300">
         {parties.map((party) => {
           const bancasTotales = sumBancas(party, 'bancas');        
           const loseBancas = sumBancas(party, 'loseBancas');      
@@ -61,36 +57,47 @@ export default function HorizontalBar() {
           const bancasEnJuegoNoRenovadas = loseBancas - renewBancas;       
 
           return (
-            <Tooltip key={party}>
-              <TooltipTrigger asChild>
-                <div
-                  className="flex hover:brightness-125"
-                  style={{ width: `${(bancasTotales / totalBancas) * 100}%` }}
-                >
-                  <div
-                    className={`bg-${colorMap[party]}-500 h-full`}
-                    style={{ width: `${(bancasNoEnJuego / bancasTotales) * 100}%` }}
-                  />
-                  <div
-                    className={`bg-${colorMap[party]}-700 h-full`}
-                    style={{ width: `${(renewBancas / bancasTotales) * 100}%` }}
-                  />
-                  <div
-                    className={`bg-${colorMap[party]}-200 h-full`}
-                    style={{ width: `${(bancasEnJuegoNoRenovadas / bancasTotales) * 100}%` }}
-                  />
-                </div>
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>
-                  {party}: {bancasTotales} totales, {renewBancas} renovadas, {loseBancas} en juego
+            <div
+              key={party}
+              className="flex hover:brightness-125 cursor-pointer relative"
+              style={{ width: `${(bancasTotales / totalBancas) * 100}%` }}
+              onMouseEnter={() => setSelectedParty(party)}
+              onMouseLeave={() => setSelectedParty(null)}
+            >
+            <div
+              className={`bg-${colorMap[party]}-500 h-full`}
+              style={{ width: `${(bancasNoEnJuego / bancasTotales) * 100}%` }}
+            />
+            <div
+              className={`bg-${colorMap[party]}-700 h-full`}
+              style={{ width: `${(renewBancas / bancasTotales) * 100}%` }}
+            />
+            <div
+              className={`bg-${colorMap[party]}-200 h-full`}
+              style={{ width: `${(bancasEnJuegoNoRenovadas / bancasTotales) * 100}%` }}
+            />
+            {/* Cuadro informativo al hacer clic */}
+            {selectedParty === party && (
+              <div
+                className="absolute border px-1 text-white bg-blue-950 shadow-lg z-10"
+                style={{
+                  bottom: "100%", // Posiciona el cuadro justo arriba de la barra
+                  marginBottom: "4px", // Espacio entre el cuadro y la barra
+                  minWidth: "400px",
+                  textAlign: "center",
+                }}
+              >
+                <p className="text-sm">
+                  {party.charAt(0).toUpperCase() + party.slice(1)}: {bancasTotales} totales,{" "}
+                  {renewBancas} renovadas, {loseBancas} en juego
                 </p>
-              </TooltipContent>
-            </Tooltip>
+              </div>
+            )}
+          </div>
           );
         })}
       </div>
-      </TooltipProvider>
     </div>
   );
 }
+
