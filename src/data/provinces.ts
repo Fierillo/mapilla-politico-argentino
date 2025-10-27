fadb4e385378f9d1c4c77277c547d9d8f130fb65
Fierillo <31271916+Fierillo@users.noreply.github.com>
2025-10-26 23:57:29 -0300

    fix: update National bancas

--- src/data/provinces.ts
+++ src/data/provinces.ts
@@ -68,15 +68,15 @@ export const partyKeys: (keyof Party)[] = [
           },
           socialismo: {
               deputies: {bancas: 5, loseBancas: 4, renewBancas: 3},
-              senate: {bancas: 0, loseBancas: 0, renewBancas: 0}
+              senate: {bancas: 0, loseBancas: 0}
           },
           centro: {
               deputies: {bancas: 103, loseBancas: 63, renewBancas: 17},
-              senate: {bancas: 27, loseBancas: 8}
+              senate: {bancas: 27, loseBancas: 8, renewBancas: 4}
           },
           liberalismo: {
               deputies: {bancas: 39, loseBancas: 8, renewBancas: 64},
-              senate: {bancas: 6, loseBancas: 0}
+              senate: {bancas: 6, loseBancas: 0, renewBancas: 13}
           }
         }  
     },
@@ -92,19 +92,19 @@ export const partyKeys: (keyof Party)[] = [
         parties: {
           peronismo: {
               deputies: {bancas: 38, loseBancas: 20, renewBancas: 21},
-              senate: {bancas: 21, loseBancas: 10, renewBancas: 7}
+              senate: {bancas: 21, loseBancas: 0 }
           },
           socialismo: {
               deputies: {bancas: 2, loseBancas: 2, renewBancas: 2},
-              senate: {bancas: 0, loseBancas: 0}
+              senate: {bancas: 0, loseBancas: 0 }
           },
           centro: {
               deputies: {bancas: 28, loseBancas: 13, renewBancas: 5},
-              senate: {bancas: 13, loseBancas: 6, renewBancas: 4}
+              senate: {bancas: 13, loseBancas: 0 }
           },
           liberalismo: {
               deputies: {bancas: 24, loseBancas: 11, renewBancas: 18},
-              senate: {bancas: 12, loseBancas: 5, renewBancas: 13}
+              senate: {bancas: 12, loseBancas: 0 }
           }
         }  
     },
